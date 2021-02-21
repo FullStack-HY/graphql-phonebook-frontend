1ccd57d1b150ab4b2cb54ce150428cb90541fea6
Matti Luukkainen <mluukkai@iki.fi>
2021-02-21 16:29:41 +0200

    part8-3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons  {
-      name
-      phone
-      id
-    }
+import { ALL_PERSONS } from './queries'
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
   }
-`
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
   if (result.loading)  {
     return <div>loading...</div>
@@ -23,8 +34,9 @@ const App = () => {
 
   return (
     <div>
-      <Persons persons = {result.data.allPersons}/>
-      <PersonForm />
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify} />
     </div>
   )
 }
