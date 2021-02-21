0b43eded82969ee72934a7899da0783579adc198
Matti Luukkainen <mluukkai@iki.fi>
2021-02-21 16:41:56 +0200

    part8-4

--- src/queries.js
+++ src/queries.js
@@ -42,3 +42,16 @@ export const ALL_PERSONS = gql`
     }
   }
 `
+export const EDIT_NUMBER = gql`
+  mutation editNumber($name: String!, $phone: String!) {
+    editNumber(name: $name, phone: $phone)  {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`
