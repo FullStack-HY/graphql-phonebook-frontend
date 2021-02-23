218faf54088243da86ea501f1cee3a3b71cf18eb
Matti Luukkainen <mluukkai@iki.fi>
2021-02-23 15:19:03 +0200

    part8-7

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -21,7 +21,14 @@ const PersonForm = ({ setError }) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    createPerson({  variables: { name, phone, street, city } })
+
+    
+    createPerson({ 
+      variables: {
+        name, street, city,
+        phone: phone.length > 0 ? phone : null
+      } 
+    })
 
     setName('')
     setPhone('')
